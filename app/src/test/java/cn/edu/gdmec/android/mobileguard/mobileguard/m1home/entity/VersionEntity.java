01e9a54ed49908f31a532a82cd92ef28c2aaa2d5
HeJie47 <896419504@qq.com>
2017-10-09 17:51:01 +0800

    Initial commit

---
+++ app/src/test/java/cn/edu/gdmec/android/mobileguard/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.mobileguard.mobileguard.m1home.entity;
+
+/**
+ * Created by Swindler on 2017/9/27.
+ */
+
+public class VersionEntity {
+    /***服务器版本号*/
+    public String versioncode;
+    /***版本描述*/
+    public String description;
+    /***apk下载地址*/
+    public String apkurl;
+}
