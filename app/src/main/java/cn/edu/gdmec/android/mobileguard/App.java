501d95c0d84299120b1b241250282a826da87a05
Ton-Cai <1583153972@qq.com>
2017-10-19 11:54:38 +0800

    12

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
@@ -1,8 +1,20 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by admin on 2017/10/19.
  */
 
-public class App {
+public class App extends Application{
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        corrtctSIM();
+    }
+    public void corrtctSIM(){
+        SharedPreferences sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+    }
 }
