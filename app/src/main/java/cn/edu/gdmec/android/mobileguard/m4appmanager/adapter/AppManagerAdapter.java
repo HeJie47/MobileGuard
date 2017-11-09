85e5b02013d05d6fae3d7eab95d2325917346313
Ton-Cai <1583153972@qq.com>
2017-11-09 10:00:08 +0800

    new change

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/adapter/AppManagerAdapter.java
@@ -0,0 +1,31 @@
+package cn.edu.gdmec.android.mobileguard.m4appmanager.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by admin on 2017/11/9.
+ */
+
+public class AppManagerAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/DensityUtil.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m4appmanager.utils;
+
+/**
+ * Created by admin on 2017/11/9.
+ */
+
+public class DensityUtil {
+}
