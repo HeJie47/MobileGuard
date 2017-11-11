d6262424d90dbb33258ce5ea84e32ff7ba6a9524
Ton-Cai <1583153972@qq.com>
2017-11-12 01:39:24 +0800

    new changed

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/DensityUtil.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/utils/DensityUtil.java
@@ -1,8 +1,35 @@
 package cn.edu.gdmec.android.mobileguard.m4appmanager.utils;
 
+import android.content.Context;
+
 /**
  * Created by admin on 2017/11/9.
  */
 
 public class DensityUtil {
+    /**
+     * dip转换像素
+     */
+    public static int dip2px(Context context, float dpValue){
+        try {
+            final float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (dpValue * scale + 0.5f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (int) dpValue;
+    }
+
+    /**
+     * 像素px转换为dip
+     */
+    public static int px2dip(Context context, float pxValue){
+        try {
+            final float scale = context.getResources().getDisplayMetrics().density;
+            return (int) (pxValue * scale + 0.5f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (int) pxValue;
+    }
 }
