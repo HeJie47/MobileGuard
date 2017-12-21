a74a0994c97f68a01baca53481fa63f2bb22a52f
HeJie47 <896419504@qq.com>
2017-12-21 20:58:32 +0800

    调试的7

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/dao/NumBelongtoDao.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/dao/NumBelongtoDao.java
@@ -29,8 +29,10 @@ public class NumBelongtoDao {
 
         if (phonenumber.matches("^1[34578]\\d{9}$")){
             //手机号码的查询
-            Cursor cursor = db.rawQuery("select location from data2 where id =(select outkey from datal where id=?)",
-                    new String[]{ phonenumber.substring(0,7)});
+            Cursor cursor = db
+                    .rawQuery(
+                            "select location from data2 where id=(select outkey from data1 where id=?)",
+                            new String[] { phonenumber.substring(0, 7) });
             if (cursor.moveToNext()){
                 location = cursor.getString(0);
             }
